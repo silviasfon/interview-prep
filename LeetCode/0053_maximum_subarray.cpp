35e6682a009c87a5a554ee00f08f792d215d4c7e
Sílvia Fonseca <silviasfon@gmail.com>
2022-10-30 15:03:33 -0300

    add recently done questions

---
+++ LeetCode/0053_maximum_subarray.cpp
@@ -0,0 +1,24 @@
+int maxSubArray(vector<int>& nums) {
+    int maxSum = -10005;
+    int currSum = 0, currCount = 0;
+    
+    for(int i = 0; i < nums.size(); i++){
+        if(nums[i] < 0){
+            maxSum = max(maxSum, nums[i]);
+
+            if(currCount > 0)
+                maxSum = max(maxSum, currSum);
+            
+            currSum += nums[i];
+            
+            if(currSum < 0)
+                currCount = currSum = 0;
+        } else {
+            currCount++;
+            currSum += nums[i];
+            maxSum = max(maxSum, currSum);
+        }
+    }
+    
+    return maxSum;
+}
